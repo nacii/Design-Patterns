5a2b2823ea4257c014b077ce5934a482c05b7812
nacii <nacii@qq.com>
2022-08-08 11:42:54 +0800

    Update 01Testing Duck Code.java

--- 01Testing Duck Code.java
+++ 01Testing Duck Code.java
@@ -1,29 +1,31 @@
 public abstract class Duck {
- FlyBehavior flyBehavior;
- QuackBehavior quackBehavior;
- public Duck() { }
- public abstract void display();
- public void performFly() {
- flyBehavior.fly();
- }
- public void performQuack() {
- quackBehavior.quack();
- } 
- public void swim() {
- System.out.println("All ducks float, even decoys!");
- }
+     FlyBehavior flyBehavior;
+     QuackBehavior quackBehavior;
+     public Duck() { }
+     public abstract void display();
+     public void performFly() {
+     flyBehavior.fly();
+     }
+     public void performQuack() {
+     quackBehavior.quack();
+     } 
+     public void swim() {
+     System.out.println("All ducks float, even decoys!");
+     }
 }
 
 public interface FlyBehavior {
- public void fly();
+    public void fly();
 }
+
 public class FlyWithWings implements FlyBehavior { 
- public void fly() {
-System.out.println("I'm flying!!");
+     public void fly() {
+    System.out.println("I'm flying!!");
  }
 }
+
 public class FlyNoWay implements FlyBehavior {
- public void fly() {
- System.out.println("I can't fly");
+     public void fly() {
+     System.out.println("I can't fly");
  }
 }
